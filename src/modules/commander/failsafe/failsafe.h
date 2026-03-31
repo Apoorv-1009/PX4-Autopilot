c19ea513963d9f5719606d04eaac2365148fc15c
gguidone <gennaroguido2002@gmail.com>
2026-03-31 17:45:11 +0200

    feat(commander): add COM_PARA_ACT to configure parachute unhealthy failsafe
    Previously the parachute unhealthy failsafe was hardcoded to RTL.

--- src/modules/commander/failsafe/failsafe.h
+++ src/modules/commander/failsafe/failsafe.h
@@ -166,6 +166,14 @@ private:
 		Terminate = 5,
 	};
 
+	enum class parachute_unhealthy_failsafe_mode : int32_t {
+		Disabled = 0,
+		Warning = 1,
+		Error = 2,
+		Return = 3,
+		Land = 4,
+	};
+
 	static ActionOptions fromNavDllOrRclActParam(int param_value);
 
 	static ActionOptions fromGfActParam(int param_value);
@@ -178,6 +186,7 @@ private:
 	static ActionOptions fromPosLowActParam(int param_value);
 	static ActionOptions fromRemainingFlightTimeLowActParam(int param_value);
 	static ActionOptions fromOdidFailActParam(int param_value);
+	static ActionOptions fromParachuteActParam(int param_value);
 
 	static bool isFailsafeIgnored(uint8_t user_intended_mode, int32_t exception_mask_parameter);
 
@@ -220,7 +229,8 @@ private:
 					(ParamInt<px4::params::COM_WIND_MAX_ACT>) _param_com_wind_max_act,
 					(ParamInt<px4::params::COM_FLTT_LOW_ACT>) _param_com_fltt_low_act,
 					(ParamInt<px4::params::COM_POS_LOW_ACT>) _param_com_pos_low_act,
-					(ParamInt<px4::params::COM_ARM_ODID>) _param_com_arm_odid
+					(ParamInt<px4::params::COM_ARM_ODID>) _param_com_arm_odid,
+					(ParamInt<px4::params::COM_PARACHUTE>) _param_com_parachute_act
 				       );
 
 };
